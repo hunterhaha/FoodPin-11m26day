b87c4402b18231577360706f74718a8f234dc0ea
hunter <hunter0697@gmail>
2020-11-26 11:54:19 +0800

    Initial Commit

---
+++ FoodPin/View/RestaurantDetailHeaderView.h
@@ -0,0 +1,17 @@
+//
+//  RestaurantDetailHeaderView.h
+//  FoodPin
+//
+//  Created by NDHU_CSIE on 2020/11/26.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RestaurantDetailHeaderView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FoodPin/View/RestaurantDetailHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  RestaurantDetailHeaderView.m
+//  FoodPin
+//
+//  Created by NDHU_CSIE on 2020/11/26.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+#import "RestaurantDetailHeaderView.h"
+
+@implementation RestaurantDetailHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
